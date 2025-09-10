b66893f4eb270ba065033554831264de149a811b
Soyabali <soyabali64@gmail.com>
2025-09-10 10:18:05 +0530

    Add new icon as per requirement

--- lib/presentation/nodatavalue/NoDataValue.dart
+++ lib/presentation/nodatavalue/NoDataValue.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+
 class NoDataScreenPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return const Center(
       child: Text(
         'No Record found',
         style: TextStyle(fontSize: 24),
