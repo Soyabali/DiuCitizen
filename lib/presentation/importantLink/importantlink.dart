6261eb62632db2680e5aa4baee7fa1a1cca34f5e
Soyabali <soyabali64@gmail.com>
2025-12-19 12:10:09 +0530

    Add a dynamic import link

--- lib/presentation/importantLink/importantlink.dart
+++ lib/presentation/importantLink/importantlink.dart
@@ -3,9 +3,13 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import '../../app/generalFunction.dart';
+import '../../services/getImportentLinksRepo.dart';
 import '../aboutDiu/Aboutdiupage.dart';
 import '../birth_death/birthanddeath.dart';
+import '../complaints/raiseGrievance/onlineComplaintForm.dart';
+import '../nodatavalue/NoDataValue.dart';
 import '../resources/app_text_style.dart';
+
 
 class Importantlink extends StatefulWidget {
 
@@ -19,30 +23,19 @@ class Importantlink extends StatefulWidget {
 class _OnlineComplaintState extends State<Importantlink> {
 
   GeneralFunction generalFunction = GeneralFunction();
-
   List<Map<String, dynamic>>? emergencyTitleList;
 
   bool isLoading = true; // logic
-  String? sName, sContactNo;
+  //String? sName, sContactNo;
+  // GeneralFunction generalFunction = GeneralFunction();
 
-
-  final List<Map<String, dynamic>> itemList2 = [
-    {
-      'title': 'Birth & Death Certificate',
-    },
-    {
-      'title': 'Collectorate Diu',
-    },
-    {
-      'title': 'Electricity Bill',
-    },
-    {
-      'title': 'E-Procurement',
-    },
-    {
-      'title': 'Mamlatdar Diu',
-    },
-  ];
+  getEmergencyTitleResponse() async {
+    emergencyTitleList = await GetImportentLinksRepo().getImportentLinks(context);
+    print('------31----$emergencyTitleList');
+    setState(() {
+      isLoading = false;
+    });
+  }
 
   final List<Color> borderColors = [
     Colors.red,
@@ -64,6 +57,8 @@ class _OnlineComplaintState extends State<Importantlink> {
 
   @override
   void initState() {
+    // TODO: implement initState
+    getEmergencyTitleResponse();
     super.initState();
   }
 
@@ -74,16 +69,17 @@ class _OnlineComplaintState extends State<Importantlink> {
       child: Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
+          // statusBarColore
           systemOverlayStyle: const SystemUiOverlayStyle(
             statusBarColor: Color(0xFF12375e),
             statusBarIconBrightness: Brightness.dark, // For Android (dark icons)
             statusBarBrightness: Brightness.light, // For iOS (dark icons)
           ),
+          // backgroundColor: Colors.blu
           backgroundColor: Color(0xFF255898),
           centerTitle: true,
           leading: GestureDetector(
             onTap: (){
-              print("------back---");
               Navigator.pop(context);
             },
             child: const Icon(Icons.arrow_back_ios,
@@ -92,7 +88,7 @@ class _OnlineComplaintState extends State<Importantlink> {
           title: Padding(
             padding: EdgeInsets.symmetric(horizontal: 5),
             child: Text(
-              'Important Link',
+              'Online Complaint',
               style: AppTextStyle.font16OpenSansRegularWhiteTextStyle,
               textAlign: TextAlign.center,
             ),
@@ -100,82 +96,49 @@ class _OnlineComplaintState extends State<Importantlink> {
           //centerTitle: true,
           elevation: 0, // Removes shadow under the AppBar
         ),
-        body:
+        body: isLoading
+            ? buildShimmerList()
+            : (emergencyTitleList == null || emergencyTitleList!.isEmpty)
+            ? NoDataScreenPage()
+            :
         Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            // middleHeader(context, '${widget.name}'),
             Container(
               height: MediaQuery.of(context).size.height * 0.8, // Adjust the height as needed
               child: ListView.builder(
                 shrinkWrap: true,
-                itemCount: itemList2?.length ?? 0,
+                itemCount: emergencyTitleList?.length ?? 0,
                 itemBuilder: (context, index) {
                   final color = borderColors[index % borderColors.length];
+
                   return Column(
                     children: <Widget>[
                       Padding(
                         padding: const EdgeInsets.symmetric(vertical: 1.0),
                         child: GestureDetector(
                           onTap: () {
-                            var title = itemList2![index]['title'];
-                            if(title=="Birth & Death Certificate"){
-                             // BirthAndDeathCertificate
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        BirthAndDeathCertificate(name: "Birth & Death Certificate")),
-                              );
-                              return;
-                            }else if(title=="Collectorate Diu"){
+                            final sTitle = emergencyTitleList![index]['sTitle'];
+                            final sLink = emergencyTitleList![index]['sLink'];
+                            print("--Title--: $sTitle");
+                            print("--Link--: $sLink");
+                            Navigator.push(
+                               context,
+                                 MaterialPageRoute(builder: (context) =>
+                                     AboutDiuPage(
+                                         name: sTitle, sPageLink: sLink)),
+                               );
 
-                              var sPageName = "Collectorate Diu";
-                              var sPageLink = "https://swp.dddgov.in/collectorate-dnhdd";
-                              // AboutDiuPage(name:sPageName,sPageLink:sPageLink);
+                            // Navigator.push(
+                            //   context,
+                            //   MaterialPageRoute(
+                            //     builder: (context) =>
+                            //         OnlineComplaintForm(name:sCategoryName,iCategoryCode:iCategoryCode),
+                            //   ),
+                            // );
 
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) =>
-                                    AboutDiuPage(
-                                        name: sPageName, sPageLink: sPageLink)),
-                              );
-                              return;
-                            }else if(title=="Electricity Bill")
-                            {
-                               print("-------221--Electricity Bill--");
-                              var sPageName = "Electricity Bill";
-                              var sPageLink = "https://connect.torrentpower.com/tplcp/index.php/crCustmast/quickpay";
-
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => AboutDiuPage(name:sPageName,sPageLink:sPageLink)),
-                              );
-                              return;
-                            }else if(title=="E-Procurement"){
-                              var eProcurement="E-Procurement";
-                              print("-----320---$eProcurement");
-                              var sPageName = "E-Procurement";
-                              var sPageLink = "https://ddtenders.gov.in/nicgep/app";
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) =>
-                                    AboutDiuPage(
-                                        name: sPageName, sPageLink: sPageLink)),
-                              );
-                              return;
-                            }else{
-                              var sPageName = "Mamlatdar Diu";
-                              var sPageLink = "https://sugam.dddgov.in/mamlatdar-diu";
-                              // AboutDiuPage(name:sPageName,sPageLink:sPageLink);
-
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) =>
-                                    AboutDiuPage(
-                                        name: sPageName, sPageLink: sPageLink)),
-                              );
-                            }
-                            },
+                          },
                           child: ListTile(
                             // leading Icon
                             leading: Container(
@@ -191,10 +154,9 @@ class _OnlineComplaintState extends State<Importantlink> {
                             ),
                             // Title
                             title: Text(
-                              itemList2[index]['title']!,
+                              emergencyTitleList![index]['sTitle']!,
                               style: AppTextStyle.font14OpenSansRegularBlackTextStyle,
                             ),
-                            //  traling icon
                             trailing: Row(
                               mainAxisSize: MainAxisSize.min,
                               children: [
@@ -228,4 +190,225 @@ class _OnlineComplaintState extends State<Importantlink> {
     );
   }
 }
+// class Importantlink extends StatefulWidget {
+//
+//   final name;
+//   Importantlink({super.key, this.name});
+//
+//   @override
+//   State<Importantlink> createState() => _OnlineComplaintState();
+// }
+//
+// class _OnlineComplaintState extends State<Importantlink> {
+//
+//   GeneralFunction generalFunction = GeneralFunction();
+//
+//   List<Map<String, dynamic>>? emergencyTitleList;
+//
+//   bool isLoading = true; // logic
+//   String? sName, sContactNo;
+//
+//
+//   final List<Map<String, dynamic>> itemList2 = [
+//     {
+//       'title': 'Birth & Death Certificate',
+//     },
+//     {
+//       'title': 'Collectorate Diu',
+//     },
+//     {
+//       'title': 'Electricity Bill',
+//     },
+//     {
+//       'title': 'E-Procurement',
+//     },
+//     {
+//       'title': 'Mamlatdar Diu',
+//     },
+//   ];
+//
+//   final List<Color> borderColors = [
+//     Colors.red,
+//     Colors.blue,
+//     Colors.green,
+//     Colors.orange,
+//     Colors.purple,
+//     Colors.pink,
+//     Colors.teal,
+//     Colors.brown,
+//     Colors.cyan,
+//     Colors.amber,
+//   ];
+//
+//   Color getRandomBorderColor() {
+//     final random = Random();
+//     return borderColors[random.nextInt(borderColors.length)];
+//   }
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return WillPopScope(
+//       onWillPop: () async => false,
+//       child: Scaffold(
+//         backgroundColor: Colors.white,
+//         appBar: AppBar(
+//           systemOverlayStyle: const SystemUiOverlayStyle(
+//             statusBarColor: Color(0xFF12375e),
+//             statusBarIconBrightness: Brightness.dark, // For Android (dark icons)
+//             statusBarBrightness: Brightness.light, // For iOS (dark icons)
+//           ),
+//           backgroundColor: Color(0xFF255898),
+//           centerTitle: true,
+//           leading: GestureDetector(
+//             onTap: (){
+//               print("------back---");
+//               Navigator.pop(context);
+//             },
+//             child: const Icon(Icons.arrow_back_ios,
+//               color: Colors.white,),
+//           ),
+//           title: Padding(
+//             padding: EdgeInsets.symmetric(horizontal: 5),
+//             child: Text(
+//               'Important Link',
+//               style: AppTextStyle.font16OpenSansRegularWhiteTextStyle,
+//               textAlign: TextAlign.center,
+//             ),
+//           ),
+//           //centerTitle: true,
+//           elevation: 0, // Removes shadow under the AppBar
+//         ),
+//         body:
+//         Column(
+//           crossAxisAlignment: CrossAxisAlignment.stretch,
+//           children: <Widget>[
+//             Container(
+//               height: MediaQuery.of(context).size.height * 0.8, // Adjust the height as needed
+//               child: ListView.builder(
+//                 shrinkWrap: true,
+//                 itemCount: itemList2?.length ?? 0,
+//                 itemBuilder: (context, index) {
+//                   final color = borderColors[index % borderColors.length];
+//                   return Column(
+//                     children: <Widget>[
+//                       Padding(
+//                         padding: const EdgeInsets.symmetric(vertical: 1.0),
+//                         child: GestureDetector(
+//                           onTap: () {
+//                             var title = itemList2![index]['title'];
+//                             if(title=="Birth & Death Certificate"){
+//                              // BirthAndDeathCertificate
+//                               Navigator.push(
+//                                 context,
+//                                 MaterialPageRoute(
+//                                     builder: (context) =>
+//                                         BirthAndDeathCertificate(name: "Birth & Death Certificate")),
+//                               );
+//                               return;
+//                             }else if(title=="Collectorate Diu"){
+//
+//                               var sPageName = "Collectorate Diu";
+//                               var sPageLink = "https://swp.dddgov.in/collectorate-dnhdd";
+//                               // AboutDiuPage(name:sPageName,sPageLink:sPageLink);
+//
+//                               Navigator.push(
+//                                 context,
+//                                 MaterialPageRoute(builder: (context) =>
+//                                     AboutDiuPage(
+//                                         name: sPageName, sPageLink: sPageLink)),
+//                               );
+//                               return;
+//                             }else if(title=="Electricity Bill")
+//                             {
+//                                print("-------221--Electricity Bill--");
+//                               var sPageName = "Electricity Bill";
+//                               var sPageLink = "https://connect.torrentpower.com/tplcp/index.php/crCustmast/quickpay";
+//
+//                               Navigator.push(
+//                                 context,
+//                                 MaterialPageRoute(builder: (context) => AboutDiuPage(name:sPageName,sPageLink:sPageLink)),
+//                               );
+//                               return;
+//                             }else if(title=="E-Procurement"){
+//                               var eProcurement="E-Procurement";
+//                               print("-----320---$eProcurement");
+//                               var sPageName = "E-Procurement";
+//                               var sPageLink = "https://ddtenders.gov.in/nicgep/app";
+//                               Navigator.push(
+//                                 context,
+//                                 MaterialPageRoute(builder: (context) =>
+//                                     AboutDiuPage(
+//                                         name: sPageName, sPageLink: sPageLink)),
+//                               );
+//                               return;
+//                             }else{
+//                               var sPageName = "Mamlatdar Diu";
+//                               var sPageLink = "https://sugam.dddgov.in/mamlatdar-diu";
+//                               // AboutDiuPage(name:sPageName,sPageLink:sPageLink);
+//
+//                               Navigator.push(
+//                                 context,
+//                                 MaterialPageRoute(builder: (context) =>
+//                                     AboutDiuPage(
+//                                         name: sPageName, sPageLink: sPageLink)),
+//                               );
+//                             }
+//                             },
+//                           child: ListTile(
+//                             // leading Icon
+//                             leading: Container(
+//                                 width: 35,
+//                                 height: 35,
+//                                 decoration: BoxDecoration(
+//                                   color: color, // Set the dynamic color
+//                                   borderRadius: BorderRadius.circular(5),
+//                                 ),
+//                                 child: const Icon(Icons.ac_unit,
+//                                   color: Colors.white,
+//                                 )
+//                             ),
+//                             // Title
+//                             title: Text(
+//                               itemList2[index]['title']!,
+//                               style: AppTextStyle.font14OpenSansRegularBlackTextStyle,
+//                             ),
+//                             //  traling icon
+//                             trailing: Row(
+//                               mainAxisSize: MainAxisSize.min,
+//                               children: [
+//                                 Image.asset(
+//                                     'assets/images/arrow.png',
+//                                     height: 12,
+//                                     width: 12,
+//                                     color: color
+//                                 ),
+//                               ],
+//                             ),
+//                           ),
+//                         ),
+//                       ),
+//                       Padding(
+//                         padding: const EdgeInsets.only(left: 12, right: 12),
+//                         child: Container(
+//                           height: 1,
+//                           color: Colors.grey, // Gray color for the bottom line
+//                         ),
+//                       ),
+//                     ],
+//                   );
+//                 },
+//               ),
+//             ),
+//           ],
+//         ),
+//
+//       ),
+//     );
+//   }
+// }
 
---
+++ lib/services/getImportentLinksRepo.dart
@@ -0,0 +1,63 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../app/generalFunction.dart';
+import '../app/loader_helper.dart';
+import 'baseurl.dart';
+
+class GetImportentLinksRepo {
+
+  GeneralFunction generalFunction = GeneralFunction();
+  Future<List<Map<String, dynamic>>?> getImportentLinks(BuildContext context) async {
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? sToken = prefs.getString('sToken');
+    String? iCitizenCode = prefs.getString('iCitizenCode');
+
+    print('-----16---$sToken');
+    print('-----17---$iCitizenCode');
+    print('---token----$sToken');
+
+    try {
+      var baseURL = BaseRepo().baseurl;
+      var endPoint = "GetImportentLinks/GetImportentLinks";
+      var getimportLinkApi = "$baseURL$endPoint";
+      showLoader();
+
+      var headers = {
+        'token': '$sToken',
+        'Content-Type': 'application/json'
+      };
+      var request = http.Request('GET', Uri.parse('$getimportLinkApi'));
+
+      request.headers.addAll(headers);
+      http.StreamedResponse response = await request.send();
+      if(response.statusCode ==401){
+        generalFunction.logout(context);
+      }
+      if (response.statusCode == 200) {
+        hideLoader();
+        var data = await response.stream.bytesToString();
+        Map<String, dynamic> parsedJson = jsonDecode(data);
+        List<dynamic>? dataList = parsedJson['Data'];
+
+        if (dataList != null) {
+          List<Map<String, dynamic>> importantLinkList = dataList.cast<Map<String, dynamic>>();
+          print("xxxxx------46----: $importantLinkList");
+          return importantLinkList;
+        } else{
+          return null;
+        }
+      } else {
+        hideLoader();
+        return null;
+      }
+    } catch (e) {
+      hideLoader();
+      debugPrint("Exception: $e");
+      throw e;
+    }
+  }
+}
